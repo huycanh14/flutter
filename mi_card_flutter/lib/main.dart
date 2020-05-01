fc96d6eec9daddbb3466088269c50dbefbe38273
Huy Canh <huycanh14@gmail.com>
2020-05-01 09:27:07 +0700

    done mini card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -12,37 +12,125 @@ class MyApp extends StatelessWidget{
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            // mainAxisSize: MainAxisSize.min,
-            // verticalDirection: VerticalDirection.down,
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: 100.0,
-                // width: 100.0,
-                // margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0 ),
-                color: Colors.white,
-                child: Text('Hello'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avata.jpg'),
+              ),
+              Text(
+                'Canh Huy',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize:40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               SizedBox(
                 height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100
+                )
               ),
-              Container(
-                // width: 300.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2')
+              /*Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  color: Colors.white,
+                  // padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.all(25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        // size: 100.0
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+84 357 572 157',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        )
+                      ),
+
+                    ],),
+                  ),
+                ),
+              ),*/
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+84 357 572 157',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      )
+                    ),
+                  ),
               ),
-              Container(
-                width: double.infinity,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 3')
+              Card(
+                  color: Colors.white,
+                  // padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'canh.huy.web@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      )
+                    ),
+                  ),
+                  /*Row(children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      // size: 100.0
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'canh.huy.web@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      )
+                    ),
+
+                  ],),*/
               ),
-              Container(
-                width: double.infinity,
-                height: 10.0,
-              ),
+              
             ],
           ),
         ),
