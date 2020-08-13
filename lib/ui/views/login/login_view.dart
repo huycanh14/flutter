aa01e8b405fdf952c4ed12530075be6a1556c142
Huy Canh <huycanh14@gmail.com>
2020-08-13 22:09:56 +0700

    set background color

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -18,96 +18,108 @@ class _LoginViewState extends State<LoginView> {
             appBar: AppBar(
               title: Text("Thông tin đăng nhâp"),
             ),
-            body: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                mainAxisSize:  MainAxisSize.min,
-                children: <Widget>[
-                  Form(
-                     key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: TextFormField(
-                              controller: model.email,
-                              validator: (value) {
-                                return model.checkValidateEmail(value);
-                              },
-                              decoration: InputDecoration(
-                                labelText: "Nhập email của bạn",
-                                border: new OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25.0),
-                                  borderSide: new BorderSide(),
-                                ),
-                                icon: const Padding(
-                                    padding: const EdgeInsets.only(top: 15.0),
-                                    child: const Icon(Icons.email)),
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: TextFormField(
-                              validator: (value) {
-                                return model.checkValidatePassword(value);
-                              },
-                              decoration: InputDecoration(
-                                labelText: "Nhập mật khẩu của bạn",
-                                border: new OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25.0),
-                                  borderSide: new BorderSide(),
-                                ),
-                                icon: const Padding(
-                                    padding: const EdgeInsets.only(top: 15.0),
-                                    child: const Icon(Icons.lock)),
-                                suffixIcon: IconButton(
-                                    icon: Icon(Icons.visibility), onPressed: null),
-                              ),
-                              obscureText: true,
-                            ),
-                          ),
-                          Padding(padding: EdgeInsets.all(20)),
-                          Container(
-                              width: MediaQuery.of(context).size.width * 1,
-                              height: MediaQuery.of(context).size.height * 0.08,
-                              child: RaisedButton(
-                                onPressed: () => {
-                                  if (_formKey.currentState.validate()) {
-                                    print(model.email.text)
-                                  }
+            body: Container(
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  focal: Alignment.center,
+                  radius: 2.0,
+                  colors: [Color(0xffc9ebe6), Color(0xfff746fc)],
+                )
+              ),
+              alignment:  Alignment.center,
+              height: MediaQuery.of(context).size.height,
+              child: SingleChildScrollView(
+                child: Column( 
+                  mainAxisSize:  MainAxisSize.min,
+                  children: <Widget>[
+                    Form(
+                       key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: TextFormField(
+                                controller: model.email,
+                                validator: (value) {
+                                  return model.checkValidateEmail(value);
                                 },
-                                textColor: Colors.white,
-                                color: Colors.lightGreen,
-                                child: Text(
-                                  'Đăng nhập',
-                                  style: TextStyle(fontSize: 20),
+                                decoration: InputDecoration(
+                                  labelText: "Nhập email của bạn",
+                                  border: new OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(25.0),
+                                    borderSide: new BorderSide(),
+                                  ),
+                                  icon: const Padding(
+                                      padding: const EdgeInsets.only(top: 15.0),
+                                      child: const Icon(Icons.email)),
                                 ),
-                                padding: EdgeInsets.all(0),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                              )),
-                        ],
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: TextFormField(
+                                validator: (value) {
+                                  return model.checkValidatePassword(value);
+                                },
+                                decoration: InputDecoration(
+                                  labelText: "Nhập mật khẩu của bạn",
+                                  border: new OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(25.0),
+                                    borderSide: new BorderSide(),
+                                  ),
+                                  icon: const Padding(
+                                      padding: const EdgeInsets.only(top: 15.0),
+                                      child: const Icon(Icons.lock)),
+                                  suffixIcon: IconButton(
+                                      icon: Icon(Icons.visibility), onPressed: null),
+                                ),
+                                obscureText: true,
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.all(20)),
+                            Container(
+                                width: MediaQuery.of(context).size.width * 1,
+                                height: MediaQuery.of(context).size.height * 0.08,
+                                child: RaisedButton(
+                                  onPressed: () => {
+                                    if (_formKey.currentState.validate()) {
+                                      print(model.email.text)
+                                    }
+                                  },
+                                  textColor: Colors.white,
+                                  color: Colors.lightGreen,
+                                  child: Text(
+                                    'Đăng nhập',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  padding: EdgeInsets.all(0),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                )),
+                          ],
+                        ),
                       ),
-                    ),
-                     Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            'Đăng ký tài khoản',
-                            style: textHomeRoute,
+                      Container(
+                         child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                'Đăng ký tài khoản',
+                                style: textHomeRoute,
+                              ),
+                              Text(
+                                'Quên mật khẩu',
+                                style: textHomeRoute,
+                              )
+                            ],
                           ),
-                          Text(
-                            'Quên mật khẩu',
-                            style: textHomeRoute,
-                          )
-                        ],
                       ),
-                    ),
-                ],
+                       ),
+                  ],
+                ),
               ),
             )),
         viewModelBuilder: () => LoginViewModel());
